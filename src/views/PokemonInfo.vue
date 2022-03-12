65c9d8779a37db9d6b267c810d103c083036c20d
Eloise Ferreira <eloiseferreira045@gmail.com>
2022-03-11 21:25:35 -0300

    FEATURE: Get information by id

--- src/views/PokemonInfo.vue
+++ src/views/PokemonInfo.vue
@@ -2,25 +2,78 @@
     <div class="Page-container mx-5 d-flex ">
 
         <div class="Card-pokemon me-5 Elev5 d-flex flex-column justify-content-around align-items-center">
-            <h6 class="id">#003</h6>
-                <img class="Pokemon-img" src="../assets/img/venosaur.png">
+            <h6 class="id">#00{{id}}</h6>
+                <img class="Pokemon-img" :src="front">
         </div>
 
         <div class="Info-container text-start">
-            <h2 class="Info-title">Venosaur</h2>
-            <h5>Type: </h5>
-            <h5>Abilities: </h5>
-            <h5>Height: </h5>
-            <h5>Weight: </h5>
+            <h2 class="Info-title">{{pokeName}}</h2>
+            <h5>Type: {{type}} </h5>
+            <h5>Abilities: {{ability1}} {{ability2}}</h5>
+            <h5>Height: {{height}} </h5>
+            <h5>Weight: {{weight}}</h5>
         </div>
     </div>
 </template>
 
 <script>
+import api from '@/services/api'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
 export default {
     name: "PokemonInfo",
-    setup() {
-        
+   setup(){
+        const route = useRoute()
+        const id = route.params.id
+        const pokeName = ref([]);
+        const weight = ref ([]);
+        const height = ref ([]);
+        const type = ref ([]);
+        const front = ref ([]);
+        const ability1 = ref ([]);
+        const ability2 = ref ([]);
+        const hp = ref ([]);
+        const attack = ref ([]);
+        const defense = ref ([]);
+        const spAtk = ref ([]);
+        const spDef = ref ([]);
+        const speed = ref ([]);
+
+        api.get(`pokemon/${id}`).then((response) => {
+            pokeName.value = response.data.name;
+            weight.value = response.data.weight;
+            height.value = response.data.height;
+            type.value = response.data.types[0].type.name;
+            front.value = response.data.sprites.front_default;
+            ability1.value = response.data.abilities[0].ability.name;
+            ability2.value = response.data.abilities[1].ability.name;
+            hp.value = response.data.stats[0].base_stat;
+            attack.value = response.data.stats[1].base_stat;
+            defense.value = response.data.stats[2].base_stat;
+            spAtk.value = response.data.stats[3].base_stat;
+            spDef.value = response.data.stats[4].base_stat;
+            speed.value = response.data.stats[5].base_stat;
+    });
+
+
+        return {
+            id,
+                pokeName,
+                weight,
+                height,
+                type,
+                front,
+                ability1,
+                ability2,
+                hp,
+                attack,
+                defense,
+                spAtk,
+                spDef,
+                speed,
+             
+        }
     },
 }
 </script>
@@ -42,5 +95,6 @@ export default {
 
 .Info-title{
     font-weight: 600;
+    text-transform: uppercase;
 }
 </style>
