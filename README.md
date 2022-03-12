b1b19deca765ba8cd68924a60a4e4259936091fa
Eloise Ferreira <eloiseferreira045@gmail.com>
2022-03-11 23:45:12 -0300

    FEATURE: Upload TrainerCard and ReadMe

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-# pokedex
+# Hi, this is my PokeDex Project
 
-## Project setup
+## About this project
+
+Like so many Zillenials, I had a lot of fun watching Pokémon as a child. My favorite movie was Pokémn 2000 and I played a lot of roms with a GBA Emulator. I'm glad something I enjoy was able to teach me something <3
+
+## How to start
+
+As soons as you clone the project, use the following commands in your terminal
+
+### Install the dependencies
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
-### Lints and fixes files
-```
-npm run lint
-```
+#### Notes
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Unfortunately, I was not able to apply a mobile friendly interface in this version, but it will be done in a future commit! Thank you all!
--- src/views/TrainerCard.vue
+++ src/views/TrainerCard.vue
@@ -2,11 +2,20 @@
     <div class="Page-container mx-4">
         <h1 class="Page-title mt-5 mb-5">Trainer's Card</h1>
 
-        <div class="mt-5 d-flex justify-content-around flex-wrap">
-          
+        <div class="Info-container mt-5 me-5 d-flex">
+
+        <div class="Card-profile Elev5 d-flex align-items-center justify-content-center">
+                <img class="Profile-img align-self-center" src="../assets/img/profile-pic.png">
         </div>
 
+        <div class="Info-container text-start">
+            <h2 class="Info-title">Eloise Ferreira</h2>
+            <h5 class="mb-3">Level: 20 </h5>
+            <h5 class="mb-3">Abilities: Front-End </h5>
+            <h5 class="mb-3">You can find me <a class="link" href="https://linktr.ee/eloiseferreira">Here</a>! </h5>
+        </div>
     </div>
+        </div>
 </template>
 
 <script>
@@ -19,5 +28,27 @@ export default {
 </script>
 
 <style scoped>
+
+.Card-profile{
+    width: 20rem;
+    height: 20rem;
+    border: 3px solid #ED317C
+}
+
+.Profile-img{
+    width: 75%;
+    height: auto;
+}
+
+.Info-container{
+    margin-left: 4rem ;
+    margin-right: 4rem ; 
+}
+
+.link{
+    font-weight: bold;
+    text-decoration: none !important;
+    color: #000;
+}
 
 </style>
