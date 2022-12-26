d44800927060f9ed5ec8c818f177a9f9560245d5
EloiseFEh <eloiseferreira045@gmail.com>
2022-12-25 22:45:12 -0300

    rm trainer card from router

--- src/router/index.js
+++ src/router/index.js
@@ -13,15 +13,6 @@ const routes = [
     name: "PokemonInfo",
     component: PokemonInfo,
   },
-  {
-    path: "/trainercard",
-    name: "TrainderCard",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/TrainerCard.vue"),
-  },
 ];
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
